7ecda9c7db4c6abaa4ff8e78c510a107b3635f08
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2022-07-05 21:46:15 +1000

    Update CrashTransferPM4.php

--- src/skyss0fly/CrashTransferPM4/CrashTransferPM4.php
+++ src/skyss0fly/CrashTransferPM4/CrashTransferPM4.php
@@ -18,7 +18,7 @@ class CrashTransferPM4 extends PluginBase implements Listener {
 
     public function onLoad(): void
     {
-        $this->message = new Config(
+        $this->$message = new Config(
             $this->getFile() . "resources/languages/" . $this->getConfig()->get("language", "en") . ".yml"
         );
     }
